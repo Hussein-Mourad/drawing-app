d59e384651313817250db164a9752e6251b47a99
Hussein-Mourad <husseinmouradkassem9901@gmail.com>
2021-06-06 07:11:12 +0200

    refactoring

---
+++ src/Shapes/Rectangle.java
@@ -0,0 +1,121 @@
+package Shapes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author hussein
+ */
+public class Rectangle implements Shape {
+
+    private int width;
+    private int height;
+    float stroke = 1;
+    Color color = Color.BLACK;
+    private Point topCornerPosition;
+    private Point bottomCornerPosition;
+
+    public Rectangle(Point topCornerPosition, Point bottomCornerPosition) {
+        this.topCornerPosition = topCornerPosition;
+        this.bottomCornerPosition = bottomCornerPosition;
+    }
+
+    public Rectangle(Point topCornerPosition, Point bottomCornerPosition, float stroke) {
+        this.topCornerPosition = topCornerPosition;
+        this.bottomCornerPosition = bottomCornerPosition;
+        this.stroke = stroke;
+    }
+
+    public Rectangle(Point topCornerPosition, Point bottomCornerPosition, float stroke, Color color) {
+        this.topCornerPosition = topCornerPosition;
+        this.bottomCornerPosition = bottomCornerPosition;
+        this.stroke = stroke;
+        this.color = color;
+    }
+
+    @Override
+    public void draw(JPanel panel, Graphics2D g2D) {
+        width = (int) (bottomCornerPosition.getX() - topCornerPosition.getX());
+        height = (int) (bottomCornerPosition.getY() - topCornerPosition.getY());
+        g2D.setStroke(new BasicStroke(stroke));
+        g2D.setColor(color);
+        g2D.drawRect((int) topCornerPosition.getX(), (int) topCornerPosition.getY(), width, height);
+        panel.repaint();
+    }
+
+    public Point getTopCornerPosition() {
+        return topCornerPosition;
+    }
+
+    public void setTopCornerPosition(Point topCornerPosition) {
+        this.topCornerPosition = topCornerPosition;
+    }
+
+    public Point getBottomCornerPosition() {
+        return bottomCornerPosition;
+    }
+
+    public void setBottomCornerPosition(Point bottomCornerPosition) {
+        this.bottomCornerPosition = bottomCornerPosition;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        if (width > 0) {
+            this.width = width;
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        if (height > 0) {
+            this.height = height;
+        }
+    }
+
+    public float getStroke() {
+        return stroke;
+    }
+
+    public void setStroke(float stroke) {
+        this.stroke = stroke;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+//    public void remove() {
+//
+//    }
+    public String getPosition() {
+        return topCornerPosition.toString();
+    }
+
+    public void setPosition(Point p) {
+        topCornerPosition.setLocation(p);
+    }
+
+    public int getX() {
+        return (int) topCornerPosition.getX();
+    }
+
+    public int getY() {
+        return (int) topCornerPosition.getY();
+    }
+
+}
---
+++ src/Shapes/Square.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Shapes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author hussein
+ */
+public class Square extends Rectangle implements Shape {
+
+    int side;
+
+    public Square(Point topCornerPosition, Point bottomCornerPosition) {
+        super(topCornerPosition, bottomCornerPosition);
+    }
+
+    public Square(Point topCornerPosition, Point bottomCornerPosition, float stroke) {
+        super(topCornerPosition, bottomCornerPosition, stroke);
+    }
+
+    public Square(Point topCornerPosition, Point bottomCornerPosition, float stroke, Color color) {
+        super(topCornerPosition, bottomCornerPosition, stroke, color);
+    }
+
+    @Override
+    public void draw(JPanel panel, Graphics2D g2D) {
+        int topX = (int) super.getTopCornerPosition().getX();
+        int topY = (int) super.getTopCornerPosition().getY();
+        int bottomX = (int) super.getBottomCornerPosition().getX();
+        super.setWidth(bottomX - topX);
+        side = super.getWidth();
+        super.setHeight(side);
+        g2D.setStroke(new BasicStroke(super.getStroke()));
+        g2D.setColor(super.getColor());
+        g2D.drawRect(topX, topY, side, side);
+        panel.repaint();
+    }
+}
